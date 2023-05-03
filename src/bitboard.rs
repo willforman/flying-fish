b8bdd3bd4357fd6f45e9205dabab336580d45518
willforman <wf8581@gmail.com>
2023-05-03 19:28:17 -0400

    update bitboard

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,59 +1,61 @@
 // use std::ops::Index;
 use std::fmt;
 
+use std::string::ToString;
+use strum_macros::{EnumIter,FromRepr,Display};
+use strum::IntoEnumIterator;
+
 #[allow(dead_code)]
 #[rustfmt::skip]
 #[repr(u8)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, EnumIter, FromRepr, Display)]
 pub(crate) enum Square {
-    A1, A2, A3, A4, A5, A6, A7, A8,
-    B1, B2, B3, B4, B5, B6, B7, B8,
-    C1, C2, C3, C4, C5, C6, C7, C8,
-    D1, D2, D3, D4, D5, D6, D7, D8,
-    E1, E2, E3, E4, E5, E6, E7, E8,
-    F1, F2, F3, F4, F5, F6, F7, F8,
-    G1, G2, G3, G4, G5, G6, G7, G8,
-    H1, H2, H3, H4, H5, H6, H7, H8,
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A8, B8, C8, D8, E8, F8, G8, H8,
 }
 
 #[derive(PartialEq, Eq)]
 pub(crate) struct BitBoard(u64);
 
 impl BitBoard {
-    fn new(squares: &[Square]) -> Self {
+    pub(crate) fn from_squares(squares: &[Square]) -> Self {
         BitBoard(
             squares
             .iter()
-            .fold(0, |board, sq| board | 1 << (63 - (*sq as u8)))
+            .fold(0, |board, sq| board | 1 << (*sq as u8))
         )
+    }
+
+    pub(crate) fn is_piece_at(&self, square: &Square) -> bool {
+        self.0 & 1 << (*square as u64) != 0
     }
 }
 
 impl fmt::Debug for BitBoard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut board_str = String::with_capacity(64 + 7);
-        for rank in 0..8 {
-            for file in (0..8).rev() {
-                let idx = rank * 8 + file;
-                let ch = if 1 << idx & self.0 != 0 {
-                    'X'
-                } else {
-                    '.'
-                };
-                board_str.push(ch);
-            }
-            if rank != 7 {
+
+        for (idx, sq) in Square::iter().rev().enumerate() {
+            let ch = if self.is_piece_at(&sq) {
+                'X'
+            } else {
+                '.'
+            };
+            board_str.push(ch);
+            if (idx + 1) % 8 == 0 && (idx + 1) != 64 {
                 board_str.push('\n');
             }
         }
+
         write!(f, "{}", board_str)
     }
 }
-
-// impl Index<Square> for BitBoard {
-//     type Output = ;
-// }
-//
 
 #[cfg(test)]
 mod tests {
@@ -61,9 +63,37 @@ mod tests {
     use super::Square::*;
 
     #[test]
-    fn test_new_bitboard() {
-        let got = BitBoard::new(&[A1, A3, A8]);
-        let want = BitBoard(0b1010000100000000000000000000000000000000000000000000000000000000);
+    fn test_bitboard_from_squares() {
+        let got = BitBoard::from_squares(&[A1, A2, E4]);
+        let want = BitBoard(0b0000000000000000000000000000000000010000000000000000000100000001);
         assert_eq!(got, want);
+    }
+
+    #[test]
+    fn test_debug() {
+        let got = BitBoard::from_squares(&[H8, G7, F6, E5, D4, C3, B2, A1]);
+        let want = "X.......\n.X......\n..X.....\n...X....\n....X...\n.....X..\n......X.\n.......X";
+        assert_eq!(format!("{:?}", got), want);
+    }
+
+    #[test]
+    fn test_is_piece_at() {
+        let bb = BitBoard::from_squares(&[A4]);
+        for sq in Square::iter() {
+            if sq == A4 {
+                assert!(bb.is_piece_at(&sq));
+            } else {
+                assert!(!bb.is_piece_at(&sq));
+            }
+        }
+
+        let bb = BitBoard(0b1111111111111111111111111111111111111111111111111111111111110111);
+        for sq in Square::iter() {
+            if sq == D1 {
+                assert!(!bb.is_piece_at(&sq));
+            } else {
+                assert!(bb.is_piece_at(&sq));
+            }
+        }
     }
 }
--- src/position.rs
+++ src/position.rs
@@ -1,5 +1,12 @@
-use crate::bitboard::{BitBoard,Square};
+use std::fmt;
+use std::ops::Index;
 
+use strum::IntoEnumIterator;
+
+use crate::bitboard::{BitBoard,Square};
+use crate::bitboard::Square::*;
+
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) enum Side {
     White,
     Black
@@ -14,26 +21,83 @@ pub(crate) enum Piece {
     King
 }
 
+pub const PIECE_CHARS: [char; 6] = [
+    'p', 'n', 'b', 'r', 'q', 'k'
+];
+
 pub(crate) struct Sides {
     white: BitBoard,
     black: BitBoard
 }
 
-// impl Sides {
-//     fn start() -> Self {
-//         Self {
-//             BitBoard(0b)
-//         }
-//     }
-// }
+impl Sides {
+    fn start() -> Self {
+        Self {
+            white: BitBoard::from_squares(&[
+                A1, B1, C1, D1, E1, F1, G1, H1,
+                A2, B2, C2, D2, E2, F2, G2, H2,
+            ]),
+            black: BitBoard::from_squares(&[
+                A7, B7, C7, D7, E7, F7, G7, H7,
+                A8, B8, C8, D8, E8, F8, G8, H8,
+            ])
+        }
+    }
+}
 
 pub(crate) struct Pieces {
-    pawn: Sides,
-    knight: Sides,
-    bishop: Sides,
-    rook: Sides,
-    queen: Sides,
-    king: Sides,
+    pawns: Sides,
+    knights: Sides,
+    bishops: Sides,
+    rooks: Sides,
+    queens: Sides,
+    kings: Sides,
+}
+
+impl Pieces {
+    fn start() -> Self {
+        Self {
+            pawns: Sides {
+                white: { BitBoard::from_squares(&[A2, B2, C2, D2, E2, F2, G2, H2]) },
+                black: { BitBoard::from_squares(&[A7, B7, C7, D7, E7, F7, G7, H7]) }
+            },
+            knights: Sides {
+                white: { BitBoard::from_squares(&[B1, G1]) },
+                black: { BitBoard::from_squares(&[B8, G8]) }
+            },
+            bishops: Sides {
+                white: { BitBoard::from_squares(&[C1, F1]) },
+                black: { BitBoard::from_squares(&[C8, F8]) }
+            },
+            rooks: Sides {
+                white: { BitBoard::from_squares(&[A1, H1]) },
+                black: { BitBoard::from_squares(&[A8, H8]) }
+            },
+            queens: Sides {
+                white: { BitBoard::from_squares(&[D1]) },
+                black: { BitBoard::from_squares(&[D8]) }
+            },
+            kings: Sides {
+                white: { BitBoard::from_squares(&[E1]) },
+                black: { BitBoard::from_squares(&[E8]) }
+            },
+        }
+    }
+}
+
+impl Index<&Piece> for Pieces {
+    type Output = Sides;
+
+    fn index(&self, index: &Piece) -> &Self::Output {
+        match index {
+            Piece::Pawn => &self.pawns,
+            Piece::Knight => &self.knights,
+            Piece::Bishop => &self.bishops,
+            Piece::Rook => &self.rooks,
+            Piece::Queen => &self.queens,
+            Piece::King => &self.kings,
+        }
+    }
 }
 
 struct CastlingRights {
@@ -78,11 +142,96 @@ pub(crate) struct Position {
     pieces: Pieces,
 }
 
-// impl Position {
-//     fn start() -> Self {
-//         Self {
-//             state: State::start(),
-//             sides: 
-//         }
-//     }
-// }
+impl Position {
+    fn start() -> Self {
+        Self {
+            state: State::start(),
+            sides: Sides::start(),
+            pieces: Pieces::start(),
+        }
+    }
+
+    fn piece_type_at(&self, sq: &Square) -> Option<(Piece, Side)> {
+        if self.pieces.pawns.white.is_piece_at(sq) {
+            Some((Piece::Pawn, Side::White))
+        } else if self.pieces.knights.white.is_piece_at(sq) {
+            Some((Piece::Knight, Side::White))
+        } else if self.pieces.bishops.white.is_piece_at(sq) {
+            Some((Piece::Bishop, Side::White))
+        } else if self.pieces.rooks.white.is_piece_at(sq) {
+            Some((Piece::Rook, Side::White))
+        } else if self.pieces.queens.white.is_piece_at(sq) {
+            Some((Piece::Queen, Side::White))
+        } else if self.pieces.kings.white.is_piece_at(sq) {
+            Some((Piece::King, Side::White))
+        } else if self.pieces.pawns.black.is_piece_at(sq) {
+            Some((Piece::Pawn, Side::Black))
+        } else if self.pieces.knights.black.is_piece_at(sq) {
+            Some((Piece::Knight, Side::Black))
+        } else if self.pieces.bishops.black.is_piece_at(sq) {
+            Some((Piece::Bishop, Side::Black))
+        } else if self.pieces.rooks.black.is_piece_at(sq) {
+            Some((Piece::Rook, Side::Black))
+        } else if self.pieces.queens.black.is_piece_at(sq) {
+            Some((Piece::Queen, Side::Black))
+        } else if self.pieces.kings.black.is_piece_at(sq) {
+            Some((Piece::King, Side::Black))
+        } else {
+            None
+        }
+    }
+}
+
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut board_str = String::with_capacity(64 + 7);
+        for (idx, sq) in Square::iter().enumerate().rev() {
+            let piece_side = self.piece_type_at(&sq);
+            let ch = match piece_side {
+                Some((Piece::Pawn, ..)) => 'p',
+                Some((Piece::Knight, ..)) => 'n',
+                Some((Piece::Bishop, ..)) => 'b',
+                Some((Piece::Rook, ..)) => 'r',
+                Some((Piece::Queen, ..)) => 'q',
+                Some((Piece::King, ..)) => 'k',
+                None => '.'
+            };
+            board_str.push(match piece_side {
+                Some((_, Side::White)) => ch.to_ascii_uppercase(),
+                _ => ch,
+            });
+
+            if idx % 8 == 0 && idx != 0 {
+                board_str.push('\n');
+            }
+        }
+        write!(f, "{}", board_str)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display() {
+        let got = Position::start();
+        let want = "RNBQKBNR\nPPPPPPPP\n........\n........\n........\n........\npppppppp\nrnbqkbnr";
+
+        assert_eq!(format!("{}", got), want);
+    }
+
+    #[test]
+    fn test_state_start() {
+        let got = Position::start();
+
+        assert!(got.state.castling_rights.white_king_side);
+        assert!(got.state.castling_rights.white_queen_side);
+        assert!(got.state.castling_rights.black_king_side);
+        assert!(got.state.castling_rights.black_queen_side);
+
+        assert_eq!(got.state.half_move_counter, 0);
+        assert_eq!(got.state.en_passant_target, None);
+        assert_eq!(got.state.to_move, Side::White);
+    }
+}
