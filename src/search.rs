6f859df290a952ea752a1d5897c70747b49e505e
willforman <wf8581@gmail.com>
2023-12-19 20:15:08 -0600

    Work on min-max search

--- src/search.rs
+++ src/search.rs
@@ -1,18 +1,48 @@
 use crate::evaluation::evaluate;
-use crate::postition::{Position,Move};
+use crate::position::Side;
+use crate::position::{Position,Move};
 use crate::move_gen::{GenerateAllMoves};
 
 pub trait EvaluatePosition {
-    pub fn evaluate(position: &Position) -> f64;
+    fn evaluate(position: &Position) -> f64;
 }
 
-pub fn find_move(position: &Position, evaluate_position: &dyn EvaluatePosition, generate_moves: &dyn GenerateAllMoves) -> Move {
-    let moves = generate_moves.gen_moves(position);
+pub fn find_move(position: &Position, evaluate_position: &impl EvaluatePosition, generate_moves: &impl GenerateAllMoves, depth: u32) -> Move {
+    let (mve, _best_val) = find_move_helper(position, evaluate_position, generate_moves, 0, depth);
+    mve
+}
 
-    moves.iter()
-        .max_by_key(| &mve| {
+fn find_move_helper(position: &Position, evaluate_position: &impl EvaluatePosition, generate_moves: &impl GenerateAllMoves, curr_depth: u32, max_depth: u32) -> (Move, f64) {
+    let mut best_move: Option<Move> = None; 
+    if position.state.to_move == Side::White {
+        let mut best_val = f64::MIN;
+        for mve in generate_moves.gen_moves(position) {
             let mut move_position = position.clone();
-            move_position.make_move(mve);
-            evaluate_position.evaluate(move_position)
-        })
+            move_position.make_move(&mve).unwrap();
+            if curr_depth == (max_depth - 1) {
+                return (mve, best_val);
+            }
+            let (_mve, got_val) = find_move_helper(&move_position, evaluate_position, generate_moves, curr_depth + 1, max_depth);
+            if got_val > best_val {
+                best_val = got_val;
+                best_move = Some(mve);
+            }
+        }
+        return (best_move.expect("Should have found a move"), best_val);
+    } else {
+        let mut best_val = f64::MAX;
+        for mve in generate_moves.gen_moves(position) {
+            let mut move_position = position.clone();
+            move_position.make_move(&mve).unwrap();
+            if curr_depth == (max_depth - 1) {
+                return (mve, best_val);
+            }
+            let (_mve, got_val) = find_move_helper(&move_position, evaluate_position, generate_moves, curr_depth + 1, max_depth);
+            if got_val < best_val {
+                best_val = got_val;
+                best_move = Some(mve);
+            }
+        }
+        return (best_move.expect("Should have found a move"), best_val);
+    }
 }
