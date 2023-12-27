4ddf9c8df11ba57ac5556f8ff2db59b3f7e1b4e4
willforman <wf8581@gmail.com>
2023-12-27 00:22:45 -0600

    Set basic layout

--- web/src/routes/index/chess_board.rs
+++ web/src/routes/index/chess_board.rs
@@ -1,8 +1,24 @@
 use leptos::*;
 
+use engine::position::Position;
+
+const BG_DARK: &str = "bg-[#86A666]";
+const BG_LIGHT: &str = "bg-[#FFFFDD]";
+
 #[component]
-pub fn ChessBoard() -> impl IntoView {
+pub fn ChessBoard(position: ReadSignal<Position>) -> impl IntoView {
     view! {
-        <p>"Board here"</p>
+        <div class="grid grid-cols-8 gap-0 w-[40rem] h-[40rem]">
+            {(0..64).into_iter()
+                .map(|i| {
+                    let color = if (i % 2) == (i / 8 % 2) {
+                        BG_LIGHT
+                    } else {
+                        BG_DARK
+                    };
+                    view! { <div class=format!("w-full h-full {}", color) /> }
+                }).collect_view()
+            }
+        </div>
     }
 }
--- web/src/routes/index/mod.rs
+++ web/src/routes/index/mod.rs
@@ -3,11 +3,23 @@ use leptos::*;
 pub mod chess_board;
 
 use crate::routes::index::chess_board::ChessBoard;
+use engine::position::Position;
 
 #[component]
 pub fn IndexPage() -> impl IntoView {
+    let (position, set_position) = create_signal(Position::start());
+
     view! {
-        <h1 class="text-center">"Vs. Computer"</h1>
-        <ChessBoard />
+        <div class="grid grid-cols-5">
+            <div class="bg-gray-200 p-2">
+                <h1 class="text-xl font-bold">"vs. computer"</h1>
+            </div>
+            <div class="col-span-3 flex justify-center">
+                <ChessBoard position=position/>
+            </div>
+            <div>
+                <h3>"move generation"</h3>
+            </div>
+        </div>
     }
 }
