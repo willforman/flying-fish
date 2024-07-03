748ebb0752a87442b134085663ee9ffe2344b1a6
willforman <wf8581@gmail.com>
2024-07-03 17:27:40 -0500

    Test running uci

--- uci/src/main.rs
+++ uci/src/main.rs
@@ -7,17 +7,28 @@ use anyhow::Result;
 use engine::HYPERBOLA_QUINTESSENCE_MOVE_GEN;
 use simple_logger::SimpleLogger;
 
-use uci::{ReadUCICommand, UCICommandStdinReader, UCI};
+use uci::UCI;
 
 fn main() -> Result<()> {
     SimpleLogger::new().env().init().unwrap();
 
-    let command_reader = UCICommandStdinReader;
     let move_gen = HYPERBOLA_QUINTESSENCE_MOVE_GEN;
+    let response_writer = Arc::new(Mutex::new(io::stdout()));
 
-    let mut uci = UCI::new(move_gen, Arc::new(Mutex::new(io::stdout())));
+    let mut uci = UCI::new(move_gen, Arc::clone(&response_writer));
+
+    let mut command_str = String::new();
     loop {
-        let command = command_reader.read_uci_command().unwrap();
-        uci.handle_command(&command).unwrap();
+        io::stdin().read_line(&mut command_str)?;
+
+        let cmd_res = uci.handle_command(&command_str);
+
+        if let Err(err) = cmd_res {
+            println!("HERE!");
+            response_writer
+                .lock()
+                .unwrap()
+                .write_all(err.to_string().as_bytes())?;
+        }
     }
 }
