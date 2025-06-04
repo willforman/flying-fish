8e2018aaba3a41a7d5951ed27f46474d0e0cd969
willforman <wf8581@gmail.com>
2025-06-04 07:27:42 -0500

    Rename uci to cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,7 +9,7 @@ use engine::{HyperbolaQuintessenceMoveGen, HYPERBOLA_QUINTESSENCE_MOVE_GEN};
 use tracing::{debug, level_filters::LevelFilter, warn, Level};
 use tracing_subscriber::{layer::SubscriberExt, prelude::*, util::SubscriberInitExt, Registry};
 
-use uci::{LOGS_DIRECTORY, UCI};
+use cli::{LOGS_DIRECTORY, UCI};
 
 static MOVE_GEN: HyperbolaQuintessenceMoveGen = HYPERBOLA_QUINTESSENCE_MOVE_GEN;
 
@@ -17,9 +17,6 @@ fn main() -> Result<()> {
     enable_logging()?;
 
     let mut uci = UCI::new(MOVE_GEN);
-
-    //uci.handle_command("uci");
-    //uci.handle_command("debug on");
 
     for line in io::stdin().lock().lines().map(|r| r.unwrap()) {
         debug!("{}", line);
