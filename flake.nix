145e0619adf533044dd0f64178e965d9ed78e75c
willforman <wf8581@gmail.com>
2025-06-12 22:11:54 -0500

    Add shallow blue and cee chess to flake

--- flake.nix
+++ flake.nix
@@ -21,6 +21,51 @@
           overlays = [ (import rust-overlay) ];
         };
 
+        shallow-blue = pkgs.stdenv.mkDerivation {
+          pname = "ShallowBlue";
+          version = "2.0.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "GunshipPenguin";
+            repo = "shallow-blue";
+            rev = "a04fbd9861770c897eb566d83b0d2e3b17aa9fc0";
+            hash = "sha256-PgAwByWzDe5Blll62aLhiodvcpKKWwoodDiZc+HbD3U=";
+          };
+
+          postPatch = ''
+            sed -i '1i\#include <string>' src/option.h
+          '';
+
+          buildPhase = ''
+            make
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            mv shallowblue $out/bin
+          '';
+        };
+
+        cee-chess = pkgs.stdenv.mkDerivation {
+          pname = "CeeChess";
+          version = "1.4";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "bctboi23";
+            repo = "CeeChess";
+            rev = "3d53576ae009418eea2da61b54c963d670fb83f1";
+            hash = "sha256-twPHChinKFew4Ugsm9oDo7d73P1RyFknPyINvll1rk4=";
+          };
+
+          buildPhase = ''
+            make
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            mv bin/CeeChess-v1.4-linux $out/bin
+          '';
+        };
 
         rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
@@ -41,6 +86,8 @@
             cargo-flamegraph
 
             stockfish
+            shallow-blue
+            cee-chess
           ]
           ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
               CoreServices
