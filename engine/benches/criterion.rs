546759dd094e9110d1a372c7703b8da2ebc773ad
willforman <wf8581@gmail.com>
2025-09-02 19:56:07 -0500

    Rewrite perft benchmark to use multiple positions

--- engine/benches/criterion.rs
+++ engine/benches/criterion.rs
@@ -1,21 +1,51 @@
 use std::sync::{atomic::AtomicBool, Arc};
+use std::time::Instant;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use engine::{
     perft, search, Position, SearchParams, HYPERBOLA_QUINTESSENCE_MOVE_GEN, POSITION_EVALUATOR,
 };
 
+const PERFT_BENCHMARK_FENS_AND_DEPTHS: &[(&str, usize, &str)] = &[
+    (
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        5,
+        "starting position",
+    ),
+    (
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        4,
+        "middlegame",
+    ),
+    ("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 5, "endgame"),
+];
+
 pub fn benchmark_perft(c: &mut Criterion) {
     let mut group = c.benchmark_group("perft");
-    group.sample_size(400);
+    group.sample_size(15);
 
-    let pos = Position::start();
+    for (fen, depth, position_name) in PERFT_BENCHMARK_FENS_AND_DEPTHS.iter() {
+        let pos = Position::from_fen(fen).unwrap();
+        let bench_name = format!("perft {}", position_name);
 
-    group.bench_function("perft early game", |b| {
-        b.iter(|| {
-            perft(&pos, 4, HYPERBOLA_QUINTESSENCE_MOVE_GEN);
-        })
-    });
+        group.bench_function(&bench_name, |b| {
+            b.iter_custom(|iters| {
+                let start = Instant::now();
+                let mut total_nodes = 0;
+
+                for _ in 0..iters {
+                    let (_, nodes) = perft(&pos, *depth, HYPERBOLA_QUINTESSENCE_MOVE_GEN);
+                    total_nodes += nodes;
+                }
+
+                let elapsed = start.elapsed();
+                let nps = total_nodes as f64 / elapsed.as_secs_f64();
+                println!("{} (depth {}): {:.0} nodes/second", &bench_name, depth, nps);
+
+                elapsed
+            })
+        });
+    }
 }
 
 pub fn benchmark_search(c: &mut Criterion) {
